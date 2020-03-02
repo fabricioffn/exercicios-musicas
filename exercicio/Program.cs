f1a4d7e087388a671bfeeb9223a7af1c4f434c09
FABRICIO <fnmamao@gmail.com>
2020-03-02 20:47:27 -0300

    adiciona musica

---
+++ exercicio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.ForegroundColor = ConsoleColor.Blue;
+            string musica = @"se voce esta contente bata palma ";
+            Console.WriteLine(musica);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string refrao = @"palmas palmas palmas";
+            Console.WriteLine(refrao);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
