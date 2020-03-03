5fb8ca3d3913247dc9663fe92e3b218b7b9576cc
FABRICIO <fnmamao@gmail.com>
2020-03-02 22:18:09 -0300

    adciona titulo

--- exercicio/Program.cs
+++ exercicio/Program.cs
@@ -6,11 +6,20 @@ namespace exercicio
     {
         static void Main(string[] args)
         {
-              Console.ForegroundColor = ConsoleColor.Blue;
-            string musica = @"se voce esta contente bata palma ";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("PARA NOSSA ALEGRIA:");
+              Console.ForegroundColor = ConsoleColor.Green;
+            string musica = @"Nos galhos secos de uma árvore qualquer
+             Onde ninguém jamais pudesse imaginar
+             O Criador vê uma flor à brotar
+
+            Olhai, olhai, olhai
+            Os lírios cresceram no campo
+            E o Senhor nosso Deus
+            Os têm alimentado para nossa alegria";
             Console.WriteLine(musica);
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            string refrao = @"palmas palmas palmas";
+            Console.ForegroundColor = ConsoleColor.Red;
+            string refrao = @"Para nossa alegria..... Para nossa alegria...!!!! Para nossa alegria!!!!!";
             Console.WriteLine(refrao);
             Console.ForegroundColor = ConsoleColor.Gray;
         }
